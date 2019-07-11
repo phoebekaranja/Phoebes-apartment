9448ad93cb9a950ab100fd556fd3c00baf574b9d
phoebekaranja <phoebemurish@gmail.com>
2019-07-11 11:37:19 +0300

    installed tinymce

--- apartment/urls.py
+++ apartment/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^shelter/',include('shelter.urls'))
+    url(r'^',include('apartment.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
