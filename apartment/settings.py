6f8b51be2f1b4d6f88948f205b8c24b616ad2b5e
phoebekaranja <phoebemurish@gmail.com>
2019-07-12 15:06:32 +0300

    making some changes

--- apartment/settings.py
+++ apartment/settings.py
@@ -31,9 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+ 
     'tinymce',
     'bootstrap3',
-    'shelter',
+    'shelter.apps.ShelterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- apartment/urls.py
+++ apartment/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^',include('apartment.urls')),
+    url(r'^',include('shelter.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
